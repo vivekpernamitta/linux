a4109b98ef6248f1a624d1c53a92aae6608776a4
vivek <vivekpernamitta@yahoo.com>
2017-12-09 13:55:04 +0530

    under development  for mutex app

---
+++ drivers/virtual/concurrency/mutex_runtime/user_2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<errno.h>
+
+
+
+
+int main()
+
+{
+	int fd, i;
+	ssize_t ret;
+	char mybuf[4000];
+	
+	sleep(1);
+	// filling buffers with *'s
+	for(i=0;i<4000;i++)
+	{
+		mybuf[i] = '*';
+	}
+
+	/* open the device for  write*/
+
+	printf("%d - Opening device vivek_dev\n", getpid());
+
+	fd=open("/dev/vivek_test", O_RDWR);
+	if(fd<=0)
+	{
+		perror("ERROR in file open");
+		exit(0);
+	}
+	printf(" PID = %d\n", getpid());
+
+	for(i=0;i<4;i++){
+		
+		ret = write( fd, "__vivek__", 12);
+		ret = write( fd, mybuf, 4000);
+	}
+	close (fd);
+
+}
